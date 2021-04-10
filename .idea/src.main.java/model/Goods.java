459096bb6a3c2aa619a88d9a445ebc62197296ad
wankin <wankin.chen@corp.to8to.com>
2021-04-10 13:36:09 +0800

    第一次上传文件

---
+++ .idea/src.main.java/model/Goods.java
@@ -0,0 +1,24 @@
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 商品类
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Goods {
+
+    private String goodsId;
+
+    private String name;
+
+    private String descr;
+
+    // 测试端口
+    private int port;
+}
